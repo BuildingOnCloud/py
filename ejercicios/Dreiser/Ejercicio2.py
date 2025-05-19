4d5324cbeab03ae9b7dbbf984c495882fda3fd9a
dreiseneitor <rodriguezdreiser@gmail.com>
2025-05-19 09:40:03 -0400

    Ejercicio en SeudoCodigo para una factura

---
+++ ejercicios/Dreiser/Ejercicio2.py
@@ -0,0 +1,21 @@
+string articulo1 = "pasta dental"
+string articulo2 = "cepillo de dientes"
+string articulo3 = "enguaje bucal"
+
+float precio1 = 10.5
+float precio2 = 8
+float precio3 = 14.7
+
+float subtotal = precio1 + precio2 + precio3
+float totaliva = subtotal * 0.15 
+float total = subtotal + totaliva
+
+print("articulo 1: " articulo1 "precio: " precio1 )
+print("articulo 2: " articulo2 "precio: " precio2 )
+print("articulo 3: " articulo3 "precio: " precio3 )
+
+print("Subtotal: " subtotal)
+
+print("Total IVA: " totaliva)
+
+print("Total: " total)
