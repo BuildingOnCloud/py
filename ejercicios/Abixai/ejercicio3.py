32a6a806431df8877acd5cd967ae416ac4c21eee
Abixai Prin <dev.abixai.martinez@gmail.com>
2025-05-22 21:10:16 -0400

    ejercicio3 calculadora Abixai2

--- ejercicios/Abixai/ejercicio3.py
+++ ejercicios/Abixai/ejercicio3.py
@@ -6,9 +6,19 @@ resultado = float (0.0)
 
 print("Cifra 1:")
 valor1 = float(input("ingresa tu primera cifra "))
-
 print("Cifra 2:")
 valor2 = float(input("ingresa tu segunda cifra "))
+
+if valor1 or valor2 == "" :
+            print("This cannot be blank. Please re-enter.")
+
+elif valor1.isdigit() == False:
+            print("Must be numbers only. Please re-enter.")
+
+elif valor2.isdigit() == False:
+            print("Must be numbers only. Please re-enter.")
+
+
 
 print("Selecciona la operación a realizar")
 
@@ -19,7 +29,11 @@ print("4. División (/)")
 
 operacion = input("Mi operación es: ")
 
-if operacion  == "1" :
+
+if operacion == "" :
+            print("This cannot be blank. Please re-enter.")
+
+elif operacion  == "1" :
 
     resultado = valor1+valor2
     print(f"Su resultado es: {resultado}")
