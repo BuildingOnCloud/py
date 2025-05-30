10ffe490de2f8db95a40e23598b559d41944a07d
Abixai Prin <dev.abixai.martinez@gmail.com>
2025-05-29 20:41:30 -0400

    ejercicio5 abixai

--- ejercicios/Abixai/ejercicio5.py
+++ ejercicios/Abixai/ejercicio5.py
@@ -5,10 +5,7 @@
 # Utilize listas para almacenar las tareas.
 
 
-toDoList[]
-
-
-
+toDolist=[]
 # Ciclamos mientras mientras el usuario no eliga Salir
 while True:
     print("\nGestión de Tareas")
@@ -18,12 +15,42 @@ while True:
     print("4. Eliminar Tarea")
     print("5. Salir")
 
-    opcion = input("Selecciona una opción (1/2/3/4/5): ")
+    opcion = int(input("Selecciona una opción (1/2/3/4/5): ")) #Agregar validacion para string 
 
-    elif opcion == "5": # Salir
+    if opcion == 1:   
+        toDolist.append(input("Escriba el nombre de la tarea: "))
+
+    elif opcion == 2:
+        if len(toDolist)>0:     #Colocar esta validacion donde se requiera imprimir la lista
+            print("Tareas pendientes:")
+            #print(toDolist)
+            index = 1
+            for tarea in toDolist:
+                print(str(index) +" "+tarea)
+                index += 1
+        else:
+            print("Lista de tareas Vacia")
+    elif opcion == 3:           #Validar que el indice ingresado exista dentro de la lista, ademas validar contra alfanumerico
+        index = 1
+        for tarea in toDolist:
+            print(str(index) +" "+tarea)
+            index += 1
+        tareaEliminar = input(" Ingrese el indice de la tarea a completar: ")
+        toDolist.pop(int(tareaEliminar)-1)
+        
+        #del toDolist[index-1]
+        #toDolist.remove(index-1)
+    elif opcion == 4:           #Validar que el indice ingresado exista dentro de la lista, ademas validar contra alfanumerico
+        index = 1
+        for tarea in toDolist:
+            print(str(index) +" "+tarea)
+            index += 1
+        tareaEliminar = input(" Ingrese el indice de la tarea a Eliminar: ")
+        toDolist.pop(int(tareaEliminar)-1)
+    elif opcion == 5: # Salir
 
         print("¡Hasta luego!")
-    break
+        break
 
     else:   # Opcion de Menu No Valida
 
